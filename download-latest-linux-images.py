39de5a27255b14b77afaa26cd0acb9bfe9b65cce
Daniel Niasoff <4070285+dniasoff@users.noreply.github.com>
2022-10-03 21:07:58 +0000

    updated

--- download-latest-linux-images.py
+++ download-latest-linux-images.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from tabnanny import check
 import requests
@@ -12,6 +12,29 @@ import guestfs
 import sys
 
 from jsonpath_ng import jsonpath, parse
+from os import environ as env
+import glanceclient.v2.client as glclient
+import keystoneclient.v3.client as ksclient
+
+from os_client_config import config as cloud_config
+
+from openstackclient.common import clientmanager
+
+
+
+keystone = ksclient.Client(auth_url=env['OS_AUTH_URL'],
+                           username=env['OS_USERNAME'],
+                           password=env['OS_PASSWORD'],
+                           tenant_name=env['OS_TENANT_NAME'],
+                           region_name=env['OS_REGION_NAME'])
+
+
+glance_endpoint = keystone.service_catalog.url_for(service_type='image')
+glance = glclient.Client(glance_endpoint, token=keystone.auth_token)
+
+
+
+currentPath = os.path.dirname(os.path.realpath(__file__))
 
 class ImageType(Enum):
     centos_stream = 1
@@ -19,6 +42,7 @@ class ImageType(Enum):
     fedora_core = 3
     debian = 4
     ubuntu = 5
+
 
 class Image(object):
     def __init__(self, version, imageType: ImageType):
@@ -79,118 +103,151 @@ def get_checksum(checksum_url, fileName, imageType: ImageType):
     return checksum
 
 for image in ImageArray:
-    imageName = "Fedora-Cloud-36"
-    imageUrl = "https://fedora.mirrorservice.org/fedora/linux/releases/36/Cloud/x86_64/images/Fedora-Cloud-Base-36-1.5.x86_64.qcow2"
-    fileName = "Fedora-Cloud-Base-36-1.5.x86_64.qcow2"
-    checksum = "ca9e514cc2f4a7a0188e7c68af60eb4e573d2e6850cc65b464697223f46b4605"
-    # imageName = ""
-    # imageUrl = ""
-    # fileName = ""
-    # checksum = ""
-    # if image.imageType == ImageType.centos_stream:
-    #     url = "https://cloud.centos.org/centos/{}-stream/x86_64/images/".format(image.version)
-    #     startsWith = "https://cloud.centos.org/centos/{}-stream/x86_64/images/CentOS-Stream-GenericCloud-{}".format(image.version, image.version)
-    #     imagePath = get_image_path(url, startsWith)
-    #     imageUrl = imagePath[0]
-    #     fileName = os.path.basename(imageUrl)
-    #     checksum = get_checksum(imagePath[1],fileName, image.imageType)
-    #     imageName = "CentOS-Stream-{}".format(image.version)
-    # if image.imageType == ImageType.fedora:
-    #     url = "https://fedora.mirrorservice.org/fedora/linux/releases/{}/Cloud/x86_64/images/".format(image.version)
-    #     startsWith = "https://fedora.mirrorservice.org/fedora/linux/releases/36/Cloud/x86_64/images/Fedora-Cloud-Base-{}".format(image.version)
-    #     imagePath = get_image_path(url, startsWith)
-    #     imageUrl = imagePath[0]
-    #     fileName = os.path.basename(imageUrl)
-    #     checksum = get_checksum(imagePath[1],fileName, image.imageType)
-    #     imageName = "Fedora-Cloud-{}".format(image.version)
-    # if image.imageType == ImageType.fedora_core:
-    #     url = "https://builds.coreos.fedoraproject.org/streams/{}.json".format(image.version)
-    #     response = requests.get(url)
-    #     if response.ok:
-    #         response_text = response.text
-    #     else:
-    #         raise Exception(response.raise_for_status())
-    #     json_data = json.loads(response.content)
+    # imageName = "Fedora-Cloud-36"
+    # imageUrl = "https://fedora.mirrorservice.org/fedora/linux/releases/36/Cloud/x86_64/images/Fedora-Cloud-Base-36-1.5.x86_64.qcow2"
+    # fileName = "Fedora-Cloud-Base-36-1.5.x86_64.qcow2"
+    # checksum = "ca9e514cc2f4a7a0188e7c68af60eb4e573d2e6850cc65b464697223f46b4605"
+    os_distro = ""
+    imageName = ""
+    imageUrl = ""
+    fileName = ""
+    checksum = ""
+    
+    if image.imageType == ImageType.centos_stream:
+        url = "https://cloud.centos.org/centos/{}-stream/x86_64/images/".format(image.version)
+        startsWith = "https://cloud.centos.org/centos/{}-stream/x86_64/images/CentOS-Stream-GenericCloud-{}".format(image.version, image.version)
+        imagePath = get_image_path(url, startsWith)
+        imageUrl = imagePath[0]
+        fileName = os.path.basename(imageUrl)
+        checksum = get_checksum(imagePath[1],fileName, image.imageType)
+        imageName = "CentOS-Stream-{}".format(image.version)
+        os_distro = "centos"
 
-    #     jsonpath_expression = parse('$.architectures.x86_64.artifacts.openstack.release')
-    #     match = jsonpath_expression.find(json_data)
-    #     version = match[0].value
-    #     imageName = "Fedora-Core-{}".format(version.split(".")[0])
-
-    #     jsonpath_expression = parse('$.architectures.x86_64.artifacts.openstack.formats["qcow2.xz"].disk.location')
-    #     match = jsonpath_expression.find(json_data)
-    #     imageUrl = match[0].value
-    #     fileName = os.path.basename(imageUrl)
-
-    #     jsonpath_expression = parse('$.architectures.x86_64.artifacts.openstack.formats["qcow2.xz"].disk["uncompressed-sha256"]')
-    #     match = jsonpath_expression.find(json_data)
-    #     checksum = match[0].value
-    # if image.imageType == ImageType.debian:
-    #     imageName = "Debian-{}".format(image.version)
-    #     imageUrl = "https://cloud.debian.org/cdimage/cloud/OpenStack/current-{}/debian-{}-openstack-amd64.qcow2".format(image.version, image.version)
-    #     fileName = os.path.basename(imageUrl)
-    #     checksum = get_checksum("https://cloud.debian.org/cdimage/cloud/OpenStack/current-{}/SHA256SUMS".format(image.version),fileName, image.imageType)
-    # if image.imageType == ImageType.ubuntu:
-    #     versionNumber = image.version.split("-")[0]
-    #     versionName = image.version.split("-")[1]
-    #     imageName = "Ubuntu-{}-{}".format(versionNumber, versionName.capitalize())
-    #     imageUrl = "https://cloud-images.ubuntu.com/{}/current/{}-server-cloudimg-amd64.img".format(versionName, versionName)
-    #     fileName = os.path.basename(imageUrl)
-    #     checksum = get_checksum("https://cloud-images.ubuntu.com/{}/current/SHA256SUMS".format(versionName),fileName, image.imageType)
     if image.imageType == ImageType.fedora:
-        tmpLocation="/tmp/{}".format(fileName)
-        # if os.path.exists(tmpLocation):
-        #     os.remove(tmpLocation)
-        # if fileName.endswith(".xz") and os.path.exists(tmpLocation[:-3]):
-        #     os.remove(tmpLocation[:-3])
-        # os.system("curl -s {} --output {}".format(imageUrl, tmpLocation))
-        # if (fileName.endswith(".xz")):
-        #     os.system("xz -d {}".format(tmpLocation))
-        #     tmpLocation=tmpLocation[:-3]
-        sha256sumOutput=subprocess.run(['sha256sum', tmpLocation], stdout=subprocess.PIPE)
-        sha256sum=sha256sumOutput.stdout.decode('UTF-8').split("\n")[0].split(" ")[0]
-        if (checksum != sha256sum):
-            raise Exception("{} checksum is not {}".format(tmpLocation, checksum))
+        url = "https://fedora.mirrorservice.org/fedora/linux/releases/{}/Cloud/x86_64/images/".format(image.version)
+        startsWith = "https://fedora.mirrorservice.org/fedora/linux/releases/36/Cloud/x86_64/images/Fedora-Cloud-Base-{}".format(image.version)
+        imagePath = get_image_path(url, startsWith)
+        imageUrl = imagePath[0]
+        fileName = os.path.basename(imageUrl)
+        checksum = get_checksum(imagePath[1],fileName, image.imageType)
+        imageName = "Fedora-Cloud-{}".format(image.version)
+        os_distro = "fedora-cloud"
 
+    if image.imageType == ImageType.fedora_core:
+        url = "https://builds.coreos.fedoraproject.org/streams/{}.json".format(image.version)
+        response = requests.get(url)
+        if response.ok:
+            response_text = response.text
+        else:
+            raise Exception(response.raise_for_status())
+        json_data = json.loads(response.content)
+
+        jsonpath_expression = parse('$.architectures.x86_64.artifacts.openstack.release')
+        match = jsonpath_expression.find(json_data)
+        version = match[0].value
+        imageName = "Fedora-Core-{}".format(version.split(".")[0])
+
+        jsonpath_expression = parse('$.architectures.x86_64.artifacts.openstack.formats["qcow2.xz"].disk.location')
+        match = jsonpath_expression.find(json_data)
+        imageUrl = match[0].value
+        fileName = os.path.basename(imageUrl)
+
+        jsonpath_expression = parse('$.architectures.x86_64.artifacts.openstack.formats["qcow2.xz"].disk["uncompressed-sha256"]')
+        match = jsonpath_expression.find(json_data)
+        checksum = match[0].value
+        os_distro = "fedora-coreos"
+
+    if image.imageType == ImageType.debian:
+        imageName = "Debian-{}".format(image.version)
+        imageUrl = "https://cloud.debian.org/cdimage/cloud/OpenStack/current-{}/debian-{}-openstack-amd64.qcow2".format(image.version, image.version)
+        fileName = os.path.basename(imageUrl)
+        checksum = get_checksum("https://cloud.debian.org/cdimage/cloud/OpenStack/current-{}/SHA256SUMS".format(image.version),fileName, image.imageType)
+        os_distro = "ubuntu"
+
+    if image.imageType == ImageType.ubuntu:
+        versionNumber = image.version.split("-")[0]
+        versionName = image.version.split("-")[1]
+        imageName = "Ubuntu-{}-{}".format(versionNumber, versionName.capitalize())
+        imageUrl = "https://cloud-images.ubuntu.com/{}/current/{}-server-cloudimg-amd64.img".format(versionName, versionName)
+        fileName = os.path.basename(imageUrl)
+        checksum = get_checksum("https://cloud-images.ubuntu.com/{}/current/SHA256SUMS".format(versionName),fileName, image.imageType)
+        os_distro = "ubuntu"
+
+
+    tmpLocation="/tmp/{}".format(fileName)
+    if os.path.exists(tmpLocation):
+        os.remove(tmpLocation)
+    if fileName.endswith(".xz") and os.path.exists(tmpLocation[:-3]):
+        os.remove(tmpLocation[:-3])
+    os.system("curl -s {} --output {}".format(imageUrl, tmpLocation))
+    if (fileName.endswith(".xz")):
+        os.system("xz -d {}".format(tmpLocation))
+        tmpLocation=tmpLocation[:-3]
+    sha256sumOutput=subprocess.run(['sha256sum', tmpLocation], stdout=subprocess.PIPE)
+    sha256sum=sha256sumOutput.stdout.decode('UTF-8').split("\n")[0].split(" ")[0]
+    if (checksum != sha256sum):
+        raise Exception("{} checksum is not {}".format(tmpLocation, checksum))
+
+    if image.imageType != ImageType.fedora_core:
         g = guestfs.GuestFS(python_return_dict=True)
-        g.add_drive_opts(tmpLocation, readonly=1)
-        # g.launch()
-        # roots = g.inspect_os()
-        # if len(roots) == 0:
-        #     print("inspect_vm: no operating systems found", file=sys.stderr)
-        #     sys.exit(1)
- 
-        # for root in roots:
-        #     print("Root device: %s" % root)
-        
-        #     # Print basic information about the operating system.
-        #     print("  Product name: %s" % (g.inspect_get_product_name(root)))
-        #     print("  Version:      %d.%d" %
-        #         (g.inspect_get_major_version(root),
-        #             g.inspect_get_minor_version(root)))
-        #     print("  Type:         %s" % (g.inspect_get_type(root)))
-        #     print("  Distro:       %s" % (g.inspect_get_distro(root)))
-        
-        #     # Mount up the disks, like guestfish -i.
-        #     #
-        #     # Sort keys by length, shortest first, so that we end up
-        #     # mounting the filesystems in the correct order.
-        #     mps = g.inspect_get_mountpoints(root)
-        #     for device, mp in sorted(mps.items(), key=lambda k: len(k[0])):
-        #         try:
-        #             g.mount_ro(mp, device)
-        #         except RuntimeError as msg:
-        #             print("%s (ignored)" % msg)
-        
-        #     # If /etc/issue.net file exists, print up to 3 lines.
-        #     filename = "/etc/issue.net"
-        #     if g.is_file(filename):
-        #         print("--- %s ---" % filename)
-        #         lines = g.head_n(3, filename)
-        #         for line in lines:
-        #             print(line)
-        
-        #     # Unmount everything.
-        g.umount_all()
- 
-        print (imageName + " " + imageUrl + " " + checksum)
+        g.add_drive_opts(tmpLocation, readonly=0)
+        g.launch()
+        roots = g.inspect_os()
+        if len(roots) == 0:
+            print("inspect_vm: no operating systems found", file=sys.stderr)
+            sys.exit(1)
+
+        for root in roots:
+            print("Root device: %s" % root)
+
+            # Print basic information about the operating system.
+            print("  Product name: %s" % (g.inspect_get_product_name(root)))
+            print("  Version:      %d.%d" %
+                (g.inspect_get_major_version(root),
+                    g.inspect_get_minor_version(root)))
+            print("  Type:         %s" % (g.inspect_get_type(root)))
+            print("  Distro:       %s" % (g.inspect_get_distro(root)))
+
+            # Mount up the disks, like guestfish -i.
+            #
+            # Sort keys by length, shortest first, so that we end up
+            # mounting the filesystems in the correct order.
+            mps = g.inspect_get_mountpoints(root)
+            for device, mp in sorted(mps.items(), key=lambda k: len(k[0])):
+                try:
+                    g.mount(mp, device)
+                except RuntimeError as msg:
+                    print("%s (ignored)" % msg)
+
+            remoteDir="/var/lib/cloud/scripts/per-once"
+            g.mkdir_p(remoteDir)
+            g.copy_in(os.path.join(currentPath,"install-vault-ssh.sh"),remoteDir)
+
+            # Unmount everything.
+            g.sync ()
+            g.umount_all()
+
+    existingId = ""
+    glanceImages = glance.images.list()
+    for glanceImage in glanceImages:
+        if glanceImage.name == imageName:
+            existingId = glanceImage.id
+
+
+    image = glance.images.create(
+        name=imageName,
+        is_public='True',
+        disk_format="qcow2",
+        container_format="bare",
+        hw_qemu_guest_agent="yes",
+        hw_rng_model="virtio",
+        hw_architecture="x86_64",
+        os_distro=os_distro
+        )
+
+    glance.images.upload(image.id, open(tmpLocation, 'rb'))
+
+    if existingId != "":
+        glance.images.delete(existingId)
+
+    print (imageName + " " + imageUrl + " " + checksum)
---
+++ install-vault-ssh.sh
@@ -0,0 +1,108 @@
+#!/usr/bin/env bash
+
+METADATA=$(curl -s http://169.254.169.254/openstack/2018-08-27/meta_data.json)
+VAULT_URL=$(echo $METADATA | awk -F'"vault_url": "' '{print $2}' | awk -F'"' '{print $1}')
+VAULT_MOUNT_POINT=$(echo $METADATA | awk -F'"vault_mount_point": "' '{print $2}' | awk -F'"' '{print $1}')
+VAULT_ALLOWED_ROLES=$(echo $METADATA | awk -F'"vault_allowed_roles": "' '{print $2}' | awk -F'"' '{print $1}')
+PROJECT_ID=$(echo $METADATA | awk -F'"project_id": "' '{print $2}' | awk -F'"' '{print $1}')
+
+
+if [ -z "$VAULT_URL" ]; then
+  VAULT_URL="https://vault.qumulus.io"
+fi
+
+if [ -z "$VAULT_MOUNT_POINT" ]; then
+  VAULT_MOUNT_POINT="openstack/ssh/${PROJECT_ID}"
+fi
+
+if [ -z "$VAULT_ALLOWED_ROLES" ]; then
+    VAULT_ALLOWED_ROLES="*"
+fi
+
+if [ ! -f /usr/local/bin/vault-ssh-helper ]; then
+    curl -s -o /usr/local/bin/vault-ssh-helper https://qumulusglobalprod.blob.core.windows.net/public-files/vault-ssh-helper
+    chmod 0755 /usr/local/bin/vault-ssh-helper
+    chown root:root /usr/local/bin/vault-ssh-helper
+fi
+
+mkdir -p /etc/vault-ssh-helper.d/
+
+cat << EOF > /etc/vault-ssh-helper.d/config.hcl
+vault_addr = "$VAULT_URL"
+tls_skip_verify = false
+ssh_mount_point = "$VAULT_MOUNT_POINT"
+allowed_roles = "$VAULT_ALLOWED_ROLES"
+EOF
+
+sed -i '/^\@include common-auth$/s/^/#/' /etc/pam.d/sshd
+sed -i '/^auth       substack     password-auth$/s/^/#/' /etc/pam.d/sshd
+grep -qxF 'auth requisite pam_exec.so expose_authtok log=/var/log/vault_ssh.log /usr/local/bin/vault-ssh-helper -config=/etc/vault-ssh-helper.d/config.hcl' /etc/pam.d/sshd || echo 'auth requisite pam_exec.so expose_authtok log=/var/log/vault_ssh.log /usr/local/bin/vault-ssh-helper -config=/etc/vault-ssh-helper.d/config.hcl' >> /etc/pam.d/sshd
+
+NOT_SET_PASS="not_set_pass "
+
+grep -q "CentOS Stream release 9" /etc/redhat-release 2> /dev/null && NOT_SET_PASS=""
+
+grep -qxF "auth optional pam_unix.so ${NOT_SET_PASS}use_first_pass nodelay" /etc/pam.d/sshd || echo "auth optional pam_unix.so ${NOT_SET_PASS}use_first_pass nodelay" >> /etc/pam.d/sshd
+
+change_line_sshd() {
+    PARAMETER=$1
+    VALUE=$2
+    if grep -q $PARAMETER /etc/ssh/sshd_config; then
+        sed -i "/.*$PARAMETER.*/d" /etc/ssh/sshd_config
+    fi
+    sed -i "1s/^/$PARAMETER $VALUE\n/" /etc/ssh/sshd_config
+}
+
+change_line_sshd ChallengeResponseAuthentication yes
+change_line_sshd PasswordAuthentication no
+change_line_sshd UsePAM yes
+change_line_sshd MaxAuthTries 15
+change_line_sshd PermitRootLogin yes
+
+SELINUX_STATUS=$(getenforce 2> /dev/null)
+
+if [ "$SELINUX_STATUS" == "Enforcing" ] ; then
+#     cat << EOF > /etc/vault-ssh-helper.d/vault-otp.te
+# module vault-otp 1.0;
+
+# require {
+#     type var_log_t;
+#     type sshd_t;
+# #    type systemd_resolved_t;
+#     type http_port_t;
+#     type container_file_t;
+#     class file open;
+#     class file create;
+#     class file read;
+#     class dir read;
+#     class tcp_socket name_connect;
+#     class lnk_file read;
+#     class lnk_file getattr;
+
+# }
+
+# #============= sshd_t ==============
+# allow sshd_t var_log_t:file open;
+# allow sshd_t var_log_t:file create;
+# allow sshd_t http_port_t:tcp_socket name_connect;
+# allow sshd_t container_file_t:dir read;
+# allow sshd_t container_file_t:file read;
+# allow sshd_t container_file_t:lnk_file read;
+# allow sshd_t container_file_t:file open;
+# allow sshd_t container_file_t:lnk_file getattr;
+
+# #============= systemd_resolved_t ==============
+# #allow systemd_resolved_t container_file_t:file read;
+
+# # references:
+# # https://github.com/hashicorp/vault-ssh-helper/issues/31#issuecomment-335565489
+# # http://www.admin-magazine.com/Articles/Credential-management-with-HashiCorp-Vault/(offset)/3
+# EOF
+
+#checkmodule -M -m -o /etc/vault-ssh-helper.d/vault-otp.mod /etc/vault-ssh-helper.d/vault-otp.te
+#semodule_package -o  /etc/vault-ssh-helper.d/vault-otp.pp -m  /etc/vault-ssh-helper.d/vault-otp.mod
+echo j/98+QEAAAABAAAAEAAAAI3/fPkPAAAAU0UgTGludXggTW9kdWxlAgAAABQAAAABAAAACAAAAAAAAAAJAAAAdmF1bHQtb3RwAwAAADEuMEAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAQAAAAKAAAAAAAAAAMAAAABAAAAAQAAAAAAAAB0Y3Bfc29ja2V0DAAAAAEAAABuYW1lX2Nvbm5lY3QDAAAAAAAAAAIAAAABAAAAAQAAAAAAAABkaXIEAAAAAQAAAHJlYWQIAAAAAAAAAAQAAAACAAAAAgAAAAAAAABsbmtfZmlsZQcAAAACAAAAZ2V0YXR0cgQAAAABAAAAcmVhZAQAAAAAAAAAAQAAAAMAAAADAAAAAAAAAGZpbGUGAAAAAgAAAGNyZWF0ZQQAAAADAAAAcmVhZAQAAAABAAAAb3BlbgEAAAABAAAACAAAAAEAAAAAAAAAb2JqZWN0X3JAAAAAAAAAAAAAAABAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAAQAAAAEAAAACwAAAAMAAAABAAAAAQAAAAAAAABAAAAAAAAAAAAAAABodHRwX3BvcnRfdAkAAAABAAAAAQAAAAEAAAAAAAAAQAAAAAAAAAAAAAAAdmFyX2xvZ190EAAAAAQAAAABAAAAAQAAAAAAAABAAAAAAAAAAAAAAABjb250YWluZXJfZmlsZV90BgAAAAIAAAABAAAAAQAAAAAAAABAAAAAAAAAAAAAAABzc2hkX3QAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEAAAABAAAAAQAAAAAAAAAAAAAACAAAAAEAAAAAAAAAQAAAAEAAAAABAAAAAAAAAAIAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAEAAAABAAAAAAQAAAAAAAAABAAAAAAAAAEAAAAAAAAAAAAAAAAAAAAABAAAAAQAAAAEAAAABAAAAAAAAAEAAAABAAAAAAQAAAAAAAAACAAAAAAAAAEAAAAAAAAAAAAAAAAAAAABAAAAAQAAAAAEAAAAAAAAAAQAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAQAAAAEAAAACAAAAAQAAAAAAAABAAAAAQAAAAAEAAAAAAAAAAgAAAAAAAABAAAAAAAAAAAAAAAAAAAAAQAAAAEAAAAABAAAAAAAAAAQAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAEAAAADAAAAAQAAAAEAAAAAAAAAQAAAAEAAAAABAAAAAAAAAAIAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAEAAAABAAAAAAQAAAAAAAAAIAAAAAAAAAEAAAAAAAAAAAAAAAAAAAAABAAAAAgAAAAEAAAABAAAAAAAAAEAAAABAAAAAAQAAAAAAAAACAAAAAAAAAEAAAAAAAAAAAAAAAAAAAABAAAAAQAAAAAEAAAAAAAAACAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAQAAAAEAAAAEAAAAAQAAAAAAAABAAAAAQAAAAAEAAAAAAAAAAgAAAAAAAABAAAAAAAAAAAAAAAAAAAAAQAAAAEAAAAABAAAAAAAAAAgAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAEAAAAEAAAAAQAAAAEAAAAAAAAAQAAAAEAAAAABAAAAAAAAAAIAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAEAAAABAAAAAAQAAAAAAAAAIAAAAAAAAAEAAAAAAAAAAAAAAAAAAAAABAAAAAQAAAAEAAAABAAAAAAAAAEAAAABAAAAAAQAAAAAAAAACAAAAAAAAAEAAAAAAAAAAAAAAAAAAAABAAAAAQAAAAAEAAAAAAAAACAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAQAAAAQAAAACAAAAAAAAAAAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAEAAAABAAAAAAQAAAAAAAAAPAAAAAAAAAEAAAAAAAAAAAAAAAEAAAABAAAAAAQAAAAAAAAAPAAAAAAAAAEAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAAQAAABAAAAAQAAAAAEAAAAAAAAABwAAAAAAAABAAAAAQAAAAAEAAAAAAAAAAQAAAAAAAABAAAAAQAAAAAEAAAAAAAAAAQAAAAAAAABAAAAAQAAAAAEAAAAAAAAAAwAAAAAAAABAAAAAAAAAAAAAAABAAAAAAAAAAAAAAABAAAAAAAAAAAAAAABAAAAAAAAAAAAAAABAAAAAAAAAAAAAAABAAAAAAAAAAAAAAABAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEAAAACgAAAHRjcF9zb2NrZXQBAAAAAQAAAAEAAAADAAAAZGlyAQAAAAEAAAABAAAACAAAAGxua19maWxlAQAAAAEAAAABAAAABAAAAGZpbGUBAAAAAQAAAAEAAAABAAAACAAAAG9iamVjdF9yAgAAAAEAAAABAAAABAAAAAsAAABodHRwX3BvcnRfdAEAAAABAAAAAQAAAAkAAAB2YXJfbG9nX3QBAAAAAQAAAAEAAAAQAAAAY29udGFpbmVyX2ZpbGVfdAEAAAABAAAAAQAAAAYAAABzc2hkX3QBAAAAAQAAAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA== | base64 -d > /etc/vault-ssh-helper.d/vault-otp.pp
+semodule -i /etc/vault-ssh-helper.d/vault-otp.pp
+fi
+
+systemctl reload sshd
